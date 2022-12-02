90bdd7d90cbd9d8f522d17f3ce1ccf3d85e70345
Lkhanaajav <Lkhanaajav.Mijiddorj-1@ou.edu>
2022-12-02 17:44:29 -0600

    adding datas

---
+++ R/spruce.R
@@ -0,0 +1,13 @@
+#' Height and Diameter of Spruce frees
+#'
+#' 144 trees
+#'
+#'
+#' @format A data frame with 144 rows and variables
+#' \describe{
+#'   \item{Height}{Height in meters}
+#'   \item{BHDiameter}{Breast Height Diameter in cm}
+#'   ...
+#' }
+#' @source \url {https://mnspruce.ornl.gov/datasets}
+"spruce"
